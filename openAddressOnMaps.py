e1ad4c0af4610fa152fda7a99cd928ed89d8ab70
EyjiCuff <eyji.koike@gmail.com>
2022-06-14 22:40:07 -0300

    Add new program

---
+++ openAddressOnMaps.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a script that takes an address as input and opens up in the browser
+the address can be in the cpliboard or the passed as argument
+"""
+
+import webbrowser, sys, pyperclip
+
+sys.argv # ['mapit.py', '870', 'Valencia', 'St.']
+
+#Check if command line arguments were passed
+if len(sys.argv) > 1:
+    # ['mapit.py', '870', 'Valencia', 'St.'] -> '870 Valencia St.'
+    address = ''.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+    
+webbrowser.open('https://www.google.com/maps/place/' + address)
+
