bfb86d86342d2f2a52dd3f7dc7a36e0a9848ef49
EyjiCuff <eyji.koike@gmail.com>
2022-06-22 10:51:06 -0300

    Improved web amazon web scraper

--- WebScraper.py
+++ WebScraper.py
@@ -4,3 +4,28 @@
 This code gets the prices from amazon webstore
 """
 
+# import the required stuff
+import bs4
+import requests
+import sys
+import pyperclip
+
+# if run from terminal, get the passed arguments
+sys.argv
+
+# if there were arguments take them as the URL, otherwise look for it on the clipboard
+if len(sys.argv) > 1:
+    productURL = sys.argv[1:]
+else:
+    productURL = pyperclip.paste()
+
+# here we defined our function
+def getAmazonPrice(URL):
+    res = requests.get(URL)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('#price')
+    return elems[0].text.strip(' ')
+
+# here we print the result back to the user
+print("The price is " + getAmazonPrice(productURL))
